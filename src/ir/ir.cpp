72ca1ea46ec0501873a274145bf0d9d3bed74c26
Vibrant <vibrant.official275@gmail.com>
2024-08-04 23:08:44 +0530

    Initial Setup

---
+++ src/ir/ir.cpp
@@ -0,0 +1,6 @@
+// Placeholder for the code generation component
+#include <iostream>
+
+void generateCode2() {
+    std::cout << "Code generation logic goes here." << std::endl;
+}
---
+++ src/parser/parser.cpp
@@ -0,0 +1,6 @@
+// Placeholder for the code generation component
+#include <iostream>
+
+void generateCode4() {
+    std::cout << "Code generation logic goes here." << std::endl;
+}
